bd5d38c30120a55c2021a618dfcc466262980e1b
UFikri <m.sidqi@rds.co.id>
2025-03-06 19:22:22 +0700

    fix: aboutme description section overflow

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -5,8 +5,7 @@ import { useRef } from "react";
 export default function AboutMe() {
   const ref = useRef(null);
 
-  const isInView = useInView(ref, { once: true }); // Aktifkan animasi saat masuk viewport
-  console.log("isInView", isInView);
+  const isInView = useInView(ref, { once: true });
 
   return (
     <section id="about" className="py-20 bg-[#0D0D0D] text-[#EDEDED] relative">
@@ -37,27 +36,30 @@ export default function AboutMe() {
         </motion.div>
 
         {/* Deskripsi */}
-        <motion.div
-          className="w-full md:w-2/3 text-center md:text-left relative z-10"
-          initial={{ opacity: 0, x: 50 }}
-          animate={isInView ? { opacity: 1, x: 0 } : {}}
-          transition={{ duration: 1 }}
-        >
-          <h2 className="text-4xl font-bold mb-4 text-[#FF5700]">About Me</h2>
-          <p className="text-lg text-gray-300">
-            Hello! I&#39;m{" "}
-            <span className="text-[#FF9100] font-semibold">Fikri Sidqi</span>, a
-            passionate Front-End Developer with more than 2 years of experience.
-            I love building interactive, beautiful, and high-performance
-            websites using <span className="text-[#FF9100]">React.js</span> and{" "}
-            <span className="text-[#FF9100]">Next.js</span>. My focus is on
-            crafting seamless user experiences with modern web technologies.
-          </p>
-          <p className="mt-4 text-lg text-gray-400">
-            Beyond coding, I enjoy exploring new technologies, mechanical
-            keyboards, and experimenting with 3D graphics. 🚀
-          </p>
-        </motion.div>
+        <div className="overflow-hidden">
+          <motion.div
+            className="w-full md:w-2/3 text-center md:text-left relative z-10"
+            initial={{ opacity: 0, x: 50 }}
+            animate={isInView ? { opacity: 1, x: 0 } : {}}
+            transition={{ duration: 1 }}
+          >
+            <h2 className="text-4xl font-bold mb-4 text-[#FF5700]">About Me</h2>
+            <p className="text-lg text-gray-300">
+              Hello! I&#39;m{" "}
+              <span className="text-[#FF9100] font-semibold">Fikri Sidqi</span>,
+              a passionate Front-End Developer with more than 2 years of
+              experience. I love building interactive, beautiful, and
+              high-performance websites using{" "}
+              <span className="text-[#FF9100]">React.js</span> and{" "}
+              <span className="text-[#FF9100]">Next.js</span>. My focus is on
+              crafting seamless user experiences with modern web technologies.
+            </p>
+            <p className="mt-4 text-lg text-gray-400">
+              Beyond coding, I enjoy exploring new technologies, mechanical
+              keyboards, and experimenting with 3D graphics. 🚀
+            </p>
+          </motion.div>
+        </div>
       </div>
     </section>
   );
