0d94a0a44cd74413b8b3ea3bfd417766c5ed0448
UFikri <m.sidqi@rds.co.id>
2025-03-07 16:59:52 +0700

    fix: allow post only contact api, add postinstall prisma generate

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -8,19 +8,22 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method Not Allowed" });
+    return res
+      .setHeader("Allow", ["POST"])
+      .status(405)
+      .json({ error: "Method Not Allowed" });
   }
 
   try {
     const { name, email, message } = req.body;
+
     const newMessage = await prisma.message.create({
       data: { name, email, message },
     });
 
     return res.status(200).json({ success: true, data: newMessage });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ error: "Something went wrong", errorMessage: error });
+    console.error("Error saving message:", error);
+    return res.status(500).json({ error: "Something went wrong" });
   }
 }
